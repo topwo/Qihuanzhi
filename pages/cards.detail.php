a3af870862223fae089aa5db151107891056488c
muyu66 <muyu.zhouyu@outlook.com>
2016-09-03 01:48:57 +0800

    basic build: 9%, gui-combination

--- pages/cards.detail.php
+++ pages/cards.detail.php
@@ -3,18 +3,34 @@
 //echo $_GET['value'];
 ?>
 
-<div id="cards-detail">
-    <div id="div_alpha"></div>
+<div id="card-detail">
+
+    <!-- UI -->
+    <div>
+        <div class="row-title">
+            <div class="row-title-img">
+                <div class="row-title-name">闭月</div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="row-pagination">
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+    <div>
+        <div class="row-close">
+            <img id="hide_this" src="../img/ui/button.close.png" />
+        </div>
+    </div>
+
+    <!-- 内容展示 -->
     <div class="swiper-container">
         <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <div class="row">
-                    <div class="img" style="top:4%;"><img src="../img/ui/cards.info.name.png" /></div>
-                    <div class="name">闭月</div>
-                </div>
-                <div class="row">
-                    <div class="left" style="top:8%;"><img src="../img/card/1.png" /></div>
-                    <div class="right" style="top:13%;">
+            <div id="single" class="swiper-slide">
+                <div class="row-info">
+                    <div class="row-info-left"><img src="../img/card/1.png" /></div>
+                    <div class="row-info-right">
                         <span>
                             <div style="float:left;color:#bcff08;"><b><small>稀有卡牌</small></b></div>
                             <div style="float:right;color:#02f8ff;"><b><small>牧师</small></b></div>
@@ -32,42 +48,186 @@
                         <span>领导力&nbsp;<b>40+5</b></span>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="detail"><img src="../img/ui/cards.info.detail.png" /></div>
-                    <div class="row-child">
-                        <span class="title"><big>普通技能</big> <b><small>射击</small></b></span>
-                        <span class="content"><small>对单体使用普通物理攻击</small></span>
-                        <span class="title"><big>主动技能</big> <b><small>剧毒射击</small></b> <b><small>等级10/10</small></b> <img src="../img/ui/cards.info.skill.png" /><img src="../img/ui/cards.info.skill2.png" /><img src="../img/ui/cards.info.skill2.png" /> </span>
-                        <span class="content"><small>降低敌方前排大量受治疗，持续2回合，不影响持续恢复技能。</small></span>
-                        <span class="title"><big>被动天赋</big> <b><small>中级防护天赋</small></b></span>
-                        <span class="content"><small>微量+韧性，大量+物抗</small></span>
-                        <span class="title"><big>队长技能</big></span>
-                        <span class="content">
+                <div class="row-skill">
+                    <div class="row-skill-block">
+                        <span class="row-skill-block-title"><span>普通技能</span> <b><small>射击</small></b></span>
+                        <span class="row-skill-block-content"><small>对单体使用普通物理攻击</small></span>
+                        <span class="row-skill-block-title">
+                            <span>主动技能</span> <b><small>剧毒射击</small></b> <b><small>等级10/10</small></b> <img src="../img/ui/cards.info.skill.png" /><img src="../img/ui/cards.info.skill2.png" /><img src="../img/ui/cards.info.skill2.png" />
+                        </span>
+                        <span class="row-skill-block-content"><small>降低敌方前排大量受治疗，持续2回合，不影响持续恢复技能。</small></span>
+                        <span class="row-skill-block-title"><span>被动天赋</span> <b><small>中级防护天赋</small></b></span>
+                        <span class="row-skill-block-content"><small>微量+韧性，大量+物抗</small></span>
+                        <span class="row-skill-block-title"><span>队长技能</span></span>
+                        <span class="row-skill-block-content">
                             <small style="color:#ff5e00;">① 宗师法力庇护</small>
                             <small style="color:#ff5e00;">① 宗师法力庇护</small>
                             <br>
                             <small style="color:#ff5e00;">① 宗师法力庇护</small>
                             <small style="color:#ff5e00;">① 宗师法力庇护</small>
                         </span>
-                        <span class="title"><big>英雄介绍</big></span>
-                        <span class="content"><small>具有深不可测的能力，四暗影和会长的师父。</small></span>
+                        <span class="row-skill-block-title"><span>英雄介绍</span></span>
+                        <span class="row-skill-block-content"><small>具有深不可测的能力，四暗影和会长的师父。</small></span>
                     </div>
                 </div>
             </div>
-            <div class="swiper-slide">Slide 2</div>
-            <div class="swiper-slide">Slide 3</div>
+            <!-- 分页 -->
+            <div id="multi" class="swiper-slide">
+                <div class="row-collect">
+                    <span class="row-collect-title"><span>牛皮的厚度</span></span>
+                    <span class="row-collect-img">
+                        <table width="100%">
+                            <tr>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="33.34%"></td>
+                            </tr>
+                        </table>
+                    </span>
+                    <span class="row-collect-content">
+                        <small class="row-collect-content-br">牛皮的厚度总是不可想象！</small>
+                        <small style="color:rgb(8,233,9);">【<b>+6% </b>物抗】 【<b>+4% </b>韧性】</small>
+                    </span>
+                </div>
+                <div class="row-collect">
+                    <span class="row-collect-title"><span>牛皮的厚度</span></span>
+                    <span class="row-collect-img">
+                        <table width="100%">
+                            <tr>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                            </tr>
+                        </table>
+                    </span>
+                    <span class="row-collect-content">
+                        <small class="row-collect-content-br">牛皮的厚度总是不可想象！</small>
+                        <small style="color:rgb(8,233,9);">【<b>+6% </b>物抗】 【<b>+4% </b>韧性】</small>
+                    </span>
+                </div>
+                <div class="row-collect">
+                    <span class="row-collect-title"><span>牛皮的厚度</span></span>
+                    <span class="row-collect-img">
+                        <table width="100%">
+                            <tr>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                            </tr>
+                        </table>
+                    </span>
+                    <span class="row-collect-content">
+                        <small class="row-collect-content-br">牛皮的厚度总是不可想象！</small>
+                        <small style="color:rgb(8,233,9);">【<b>+6% </b>物抗】 【<b>+4% </b>韧性】</small>
+                    </span>
+                </div>
+                <div class="row-collect">
+                    <span class="row-collect-title"><span>牛皮的厚度</span></span>
+                    <span class="row-collect-img">
+                        <table width="100%">
+                            <tr>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                            </tr>
+                        </table>
+                    </span>
+                    <span class="row-collect-content">
+                        <small class="row-collect-content-br">牛皮的厚度总是不可想象！</small>
+                        <small style="color:rgb(8,233,9);">【<b>+6% </b>物抗】 【<b>+4% </b>韧性】</small>
+                    </span>
+                </div>
+                <div class="row-collect">
+                    <span class="row-collect-title"><span>牛皮的厚度</span></span>
+                    <span class="row-collect-img">
+                        <table width="100%">
+                            <tr>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                            </tr>
+                        </table>
+                    </span>
+                    <span class="row-collect-content">
+                        <small class="row-collect-content-br">牛皮的厚度总是不可想象！</small>
+                        <small style="color:rgb(8,233,9);">【<b>+6% </b>物抗】 【<b>+4% </b>韧性】</small>
+                    </span>
+                </div>
+                <div class="row-collect">
+                    <span class="row-collect-title"><span>牛皮的厚度</span></span>
+                    <span class="row-collect-img">
+                        <table width="100%">
+                            <tr>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                            </tr>
+                        </table>
+                    </span>
+                    <span class="row-collect-content">
+                        <small class="row-collect-content-br">牛皮的厚度总是不可想象！</small>
+                        <small style="color:rgb(8,233,9);">【<b>+6% </b>物抗】 【<b>+4% </b>韧性】</small>
+                    </span>
+                </div>
+                <div class="row-collect">
+                    <span class="row-collect-title"><span>牛皮的厚度</span></span>
+                    <span class="row-collect-img">
+                        <table width="100%">
+                            <tr>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                            </tr>
+                        </table>
+                    </span>
+                    <span class="row-collect-content">
+                        <small class="row-collect-content-br">牛皮的厚度总是不可想象！</small>
+                        <small style="color:rgb(8,233,9);">【<b>+6% </b>物抗】 【<b>+4% </b>韧性】</small>
+                    </span>
+                </div>
+                <div class="row-collect">
+                    <span class="row-collect-title"><span>牛皮的厚度</span></span>
+                    <span class="row-collect-img">
+                        <table width="100%">
+                            <tr>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/1.png" /></td>
+                                <td width="16.67%"><img src="../img/avater/2.png" /></td>
+                            </tr>
+                        </table>
+                    </span>
+                    <span class="row-collect-content">
+                        <small class="row-collect-content-br">牛皮的厚度总是不可想象！</small>
+                        <small style="color:rgb(8,233,9);">【<b>+6% </b>物抗】 【<b>+4% </b>韧性】</small>
+                    </span>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="div_tab">
-            <div class="swiper-pagination"></div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="div_close">
-            <img src="../img/ui/button.close.png" />
-        </div>
-    </div>
+
 </div>
 
 <script>
@@ -77,8 +237,8 @@ var swiper = new Swiper('.swiper-container', {
     paginationClickable: false
 });
 
-$(".div_close img").click(function()
+$("#hide_this").click(function()
 {
-    $('#cards-detail').remove();
+    $('#card-detail').remove();
 });
 </script>
