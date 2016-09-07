192aa53feba82a88427b3eabd3f92cb65e89edf1
muyu66 <muyu.zhouyu@outlook.com>
2016-09-08 06:38:57 +0800

    basic build: 17.8%, update battle logic

--- pages/battle.php
+++ pages/battle.php
@@ -43,6 +43,9 @@ class E
 require_once '../php/redis.php';
 require_once '../php/json.php';
 
+// 常量
+define("CYCLE_COUNT", "30");
+
 // 我队卡牌
 $m_cards_all = ex(redis_get_DataArray('1.info') ['team1.cards']);
 // [key]卡牌站位，[value]图鉴ID
@@ -66,6 +69,31 @@ foreach ($e_cards_ex as $key => $value)
     $$e = new E($value);
 }
 
+// 卡牌死亡处理
+$event_die = function($m0,$m1,$m2,$m3,$m4,$m5,$e0,$e1,$e2,$e3,$e4,$e5) use($m_cards_ex,$e_cards_ex)
+{
+    $tmp = array();
+    foreach ($m_cards_ex as $key => $value)
+    {
+        $m = 'm'.$key;
+        // 获取 存在 且 生命 < 0 我方对象
+        if ( isset($$m) && $$m->hp <= 0)
+        {
+            $tmp[] = 'm'.$key;
+        }
+    }
+    foreach ($e_cards_ex as $key => $value)
+    {
+        $e = 'e'.$key;
+        // 获取 存在 且 生命 < 0 敌方对象
+        if ( isset($$e) && $$e->hp <= 0)
+        {
+            $tmp[] = 'e'.$key;
+        }
+    }
+    return $tmp;
+};
+
 // 获取 [存在] 且 [存活] 的 [敌方] [第一个] 卡牌
 $get_exist_alive_e_first = function($e0,$e1,$e2,$e3,$e4,$e5) use($e_cards_ex)
 {
@@ -82,7 +110,24 @@ $get_exist_alive_e_first = function($e0,$e1,$e2,$e3,$e4,$e5) use($e_cards_ex)
     return $tmp[0];
 };
 
-// 获取 [存在] 且 [不存活] 的 [我方] 卡牌
+// 获取 [存在] 且 [随机] 且 [存活] 的 [3个] [敌人]
+$get_exist_rand_alive_3_e = function($e0,$e1,$e2,$e3,$e4,$e5) use($e_cards_ex)
+{
+    $tmp = array();
+    foreach ($e_cards_ex as $key => $value)
+    {
+        $e = 'e'.$key;
+        // 获取 存在、存活
+        if ( isset($$e) && $$e->hp > 0)
+        {
+            $tmp[] = $key;
+        }
+    }
+    return $tmp;
+    return array_rand($tmp, 3);
+};
+
+// 获取 [存在] 且 [不存活] 的 [我方]
 $get_exist_die_m = function($m0,$m1,$m2,$m3,$m4,$m5) use($m_cards_ex)
 {
     $tmp = array();
@@ -99,16 +144,21 @@ $get_exist_die_m = function($m0,$m1,$m2,$m3,$m4,$m5) use($m_cards_ex)
 };
 
 // 主循环
-for ($c=1; $c <= 15; $c++)
+for ($c=1; $c <= CYCLE_COUNT; $c++)
 {
     echo '回合',$c,'<br>';
 
+/*
 // 死亡
 if($c == 3)
 {
     unset($m1);
     unset($m3);
+}
+if($c == 4)
+{
     unset($e0);
+    echo 'e0死了<br>';
 }
 // 复活
 if($c == 7)
@@ -116,28 +166,67 @@ if($c == 7)
     $m1 = new M($m_cards_ex[1]);
     $m3 = new M($m_cards_ex[3]);
 }
+if($c == 9)
+{
+    $e0 = new E($e_cards_ex[0]);
+    echo 'e0复活<br>';
+}
+*/
 
     // 顺序出手
     for ($c_b=0; $c_b <= 5; $c_b++)
     {
+        // 死亡即销毁对象
+        $event_die_array = $event_die(@$m0,@$m1,@$m2,@$m3,@$m4,@$m5,@$e0,@$e1,@$e2,@$e3,@$e4,@$e5);
+        foreach ($event_die_array as $key => $value)
+        {
+            unset ($$value);
+        }
+
+        // 胜负事件
+        if(!isset($e0) && !isset($e1) && !isset($e2) && !isset($e3) && !isset($e4) && !isset($e5))
+        {
+            echo '敌人死光<br>';
+            break 2;
+        }
+        if(!isset($m0) && !isset($m1) && !isset($m2) && !isset($m3) && !isset($m4) && !isset($m5))
+        {
+            echo '我方死光<br>';
+        }
+
         // 我方出手
         $m = 'm'.$c_b;
         if( isset($$m) )
         {
+
             if($c % $$m->skillActive['skill.cycle'] == $$m->skillActive['skill.start'])
             {
                 switch ($$m->skillActive['type']) {
                     case 1:
-                        $target = $get_exist_alive_e_first(@$e0,@$e1,@$e2,@$e3,@$e4,@$e5);
-                        $e = 'e'.$target;
-                        echo 'm'.$c_b.'使用'.$$m->skillActive['name'].'，对e'.$target.'，造成'.round($$m->att * $$m->skillActive['coe.att']).'伤害<br>';
-                        echo 'e'.$target.'剩余生命'.$$e->hp = $$e->hp - round($$m->att * $$m->skillActive['coe.att']).'<br>';
+                        if( $$m->skillActive['att.count'] > 1 )
+                        {
+                            // 随机攻击敌方3人
+                            foreach ($get_exist_rand_alive_3_e(@$e0,@$e1,@$e2,@$e3,@$e4,@$e5) as $key => $value)
+                            {
+                                $e = 'e'.$value;
+                                echo 'm'.$c_b.'使用'.$$m->skillActive['name'].'，对e'.$value.'，造成'.round($$m->att * $$m->skillActive['coe.att']).'伤害<br>';
+                                echo 'e'.$value.'剩余生命'.$$e->hp = $$e->hp - round($$m->att * $$m->skillActive['coe.att']).'<br>';
+                            }
+                        }
+                        elseif ( $$m->skillActive['att.count'] == 1 )
+                        {
+                            $target = $get_exist_alive_e_first(@$e0,@$e1,@$e2,@$e3,@$e4,@$e5);
+                            $e = 'e'.$target;
+                            echo 'm'.$c_b.'使用'.$$m->skillActive['name'].'，对e'.$target.'，造成'.round($$m->att * $$m->skillActive['coe.att']).'伤害<br>';
+                            echo 'e'.$target.'剩余生命'.$$e->hp = $$e->hp - round($$m->att * $$m->skillActive['coe.att']).'<br>';
+                        }
                         break;
                     case 2:
                         echo 'm'.$c_b.'释放主动技能-治疗<br>';
                         break;
                     case 3:
                         echo 'm'.$c_b.'释放主动技能-复活<br>';
+
                         // 检测死亡名单
                         //$get_exist_die_m(@$m0,@$m1,@$m2,@$m3,@$m4,@$m5);
                         break;
@@ -167,6 +256,13 @@ if($c == 7)
             echo 'e'.$c_b.'生命：'.$$e->hp.'<br>';
         }
     }
+
+    if($c == CYCLE_COUNT)
+    {
+        echo '平局<br>';
+        break 1;
+    }
+
     echo '<br><br>';
 }
 
